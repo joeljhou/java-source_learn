2d555bb10fcef76a0ffefdc69dd42b64d6542582
zhouyu <zhouyu@liquido.cn>
2024-04-11 15:50:42 +0800

    搭建一个jdk8源码学习环境

---
+++ src/cn/geekyspace/test/Main.java
@@ -0,0 +1,27 @@
+package cn.geekyspace.test;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("a", 1.0);
+        map.put("b", 2.0);
+        map.put("c", 3.0);
+        map.put("d", 4.0);
+        map.put("e", 5.0);
+        map.put("f", 6.0);
+        map.put("g", 7.0);
+        map.put("h", 8.0);
+        map.put("i", 9.0);
+        map.put("j", 10.0);
+        map.put("k", 11.0);
+        map.put("l", 12.0);
+        map.put("m", 13.0);
+        map.put("n", 14.0);
+        map.put("o", 15.0);
+        map.put("p", 16.0);
+
+        System.out.println(map);
+    }
+}
